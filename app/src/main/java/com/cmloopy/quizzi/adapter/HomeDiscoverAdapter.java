e94d64a2f299890dc0223f57a600adc85a9fa1de
Dang Minh Anh <dangminhanh4656@gmail.com>
2025-03-11 11:20:00 +0700

    Ui/MS-10 "Done"

---
+++ app/src/main/java/com/cmloopy/quizzi/adapter/HomeDiscoverAdapter.java
@@ -0,0 +1,61 @@
+package com.cmloopy.quizzi.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cmloopy.quizzi.R;
+import com.cmloopy.quizzi.models.Quiz;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+public class HomeDiscoverAdapter extends RecyclerView.Adapter<HomeDiscoverAdapter.ViewHolder> {
+    private List<Quiz> items;
+    public HomeDiscoverAdapter(List<Quiz> items) {this.items = items;}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_home_discover, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Quiz item = items.get(position);
+        holder.podcastImage.setImageResource(item.getImageResource());
+        holder.questionsText.setText(item.getQuestions());
+        holder.titleText.setText(item.getTitle());
+        holder.authorAvatar.setImageResource(item.getAuthorAvatarResource());
+        holder.authorName.setText(item.getAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ShapeableImageView podcastImage;
+        MaterialTextView titleText;
+        ShapeableImageView authorAvatar;
+        MaterialTextView authorName;
+        MaterialTextView questionsText;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            podcastImage = itemView.findViewById(R.id.img_category);
+            titleText = itemView.findViewById(R.id.txt_title);
+            authorAvatar = itemView.findViewById(R.id.img_biaAuthor);
+            authorName = itemView.findViewById(R.id.txt_name_author);
+            questionsText = itemView.findViewById(R.id.txt_ques);
+        }
+    }
+}
