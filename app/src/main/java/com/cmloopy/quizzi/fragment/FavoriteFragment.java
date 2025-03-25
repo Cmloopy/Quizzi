44b7a882caa57c9beab3398307eb72a0af7cbe94
Dang Minh Anh <dangminhanh4656@gmail.com>
2025-03-25 14:22:02 +0700

    UI/MS-32 "Favor Done"

--- app/src/main/java/com/cmloopy/quizzi/fragment/FavoriteFragment.java
+++ app/src/main/java/com/cmloopy/quizzi/fragment/FavoriteFragment.java
@@ -3,59 +3,65 @@ package com.cmloopy.quizzi.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cmloopy.quizzi.R;
+import com.cmloopy.quizzi.adapter.QuizAdapter;
+import com.cmloopy.quizzi.models.Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavoriteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private RecyclerView recyclerView;
     public FavoriteFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoriteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoriteFragment newInstance(String param1, String param2) {
-        FavoriteFragment fragment = new FavoriteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        recyclerView = view.findViewById(R.id.rcl_view_favor);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recyclerView.setAdapter(this.getQuizAdapter());
+
+        return view;
+    }
+
+    private QuizAdapter getQuizAdapter() {
+        List<Quiz> items = new ArrayList<>();
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "Get Smarter with Prod...",
+                "2 months ago", "5.5K plays", "Titus Kitamura", R.drawable.ic_launcher_background, "16 Qs"));
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "Great Ideas Come from...",
+                "6 months ago", "10.3K plays", "Alfonzo Schuessler", R.drawable.ic_launcher_background, "10 Qs"));
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "Having Fun & Always S...",
+                "2 years ago", "18.5K plays", "Daryl Nehls", R.drawable.ic_launcher_background, "12 Qs"));
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "Can You Imagine, Worl...",
+                "3 months ago", "4.9K plays", "Edgar Torrey", R.drawable.ic_launcher_background, "20 Qs"));
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "Back to School, Get Sm...",
+                "1 year ago", "12.4K plays", "Darcel Ballentine", R.drawable.ic_launcher_background, "10 Qs"));
+
+        items.add(new Quiz(R.drawable.ic_launcher_background, "What is Your Favorite ...",
+                "5 months ago", "6.2K plays", "Elmer Laverty", R.drawable.ic_launcher_background, "16 Qs"));
+        QuizAdapter quizAdapter  = new QuizAdapter(items);
+        return quizAdapter;
     }
 }
